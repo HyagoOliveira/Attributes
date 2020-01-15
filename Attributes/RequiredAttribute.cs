42fbb244a88831dc0a10b94947bbe354a09116e4
Hyago Oliveira <hyagogow@gmail.com>
2020-01-14 21:49:19 -0300

    Add RequireAttribute

---
+++ Attributes/RequiredAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// You can use this attribute on strings, exposed reference and object reference types. 
+/// If the value is null or empty, a error message will be displayed on Inspector bellow the given attribute.
+/// </summary>
+[AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+public sealed class RequiredAttribute : PropertyAttribute
+{
+    public readonly string message;
+
+    public RequiredAttribute(string message = null)
+    {
+        this.message = message;
+    }
+}
---
+++ Editor/RequiredAttributeDrawer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(RequiredAttribute))]
+public sealed class RequiredAttributeDrawer : PropertyDrawer
+{
+    private bool isValueNull = false;
+    private const float HELP_BOX_HEIGHT = 32F;
+    private const string DEFAULT_ERROR_MESSAGE_FORMAT_TEXT = "Field '{0}' requires a value!";
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        return base.GetPropertyHeight(property, label) + (isValueNull ? HELP_BOX_HEIGHT + 2f : 0f);
+    }
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        position.height = base.GetPropertyHeight(property, label);
+        EditorGUI.PropertyField(position, property, label);
+
+        if (isValueNull = IsPropertyValueNull(property))
+        {
+            RequiredAttribute requiredAttribute = attribute as RequiredAttribute;
+            string message = string.IsNullOrEmpty(requiredAttribute.message) ?
+                string.Format(DEFAULT_ERROR_MESSAGE_FORMAT_TEXT, property.name) :
+                requiredAttribute.message;
+
+            position.y += position.height + 2f;
+            position.height = HELP_BOX_HEIGHT;
+            EditorGUI.HelpBox(position, message, MessageType.Error);
+        }
+    }
+
+    private bool IsPropertyValueNull(SerializedProperty property)
+    {
+        return 
+            property.propertyType == SerializedPropertyType.String && string.IsNullOrEmpty(property.stringValue) ||
+            property.propertyType == SerializedPropertyType.ExposedReference && property.exposedReferenceValue == null ||
+            property.propertyType == SerializedPropertyType.ObjectReference && property.objectReferenceValue == null;
+    }
+}
