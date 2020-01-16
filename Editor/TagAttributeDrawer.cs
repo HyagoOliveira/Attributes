337b94ba3ec3dc6a30470502cd8045c0800257ed
Hyago Oliveira <hyagogow@gmail.com>
2020-01-16 19:22:21 -0300

    Unity package folder restructuration
    All folders and asmdef files were renamed according to Unity package naming standards

--- Editor/TagAttributeDrawer.cs
+++ Editor/TagAttributeDrawer.cs
@@ -1,48 +1,51 @@
 ﻿using UnityEditor;
 using UnityEngine;
 
-[CustomPropertyDrawer(typeof(TagAttribute))]
-public sealed class TagAttributeDrawer : PropertyDrawer
+namespace ActionCode.Attributes.Editor
 {
-    private GUIContent[] tagsContent;
-
-    private const string NOT_STRING_ERROR_FORMAT_MESSAGE = "Field '{0}' should be a string!";
-
-    public TagAttributeDrawer()
+    [CustomPropertyDrawer(typeof(TagAttribute))]
+    public sealed class TagAttributeDrawer : PropertyDrawer
     {
-        string[] tags = UnityEditorInternal.InternalEditorUtility.tags;
-        tagsContent = new GUIContent[tags.Length];
-        for (int i = 0; i < tagsContent.Length; i++)
-        {
-            tagsContent[i] = new GUIContent(tags[i]);
-        }
-    }
+        private GUIContent[] tagsContent;
 
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-    {
-        if (property.propertyType == SerializedPropertyType.String)
+        private const string NOT_STRING_ERROR_FORMAT_MESSAGE = "Field '{0}' should be a string!";
+
+        public TagAttributeDrawer()
         {
-            int index = 0;
-            string value = property.stringValue;
+            string[] tags = UnityEditorInternal.InternalEditorUtility.tags;
+            tagsContent = new GUIContent[tags.Length];
             for (int i = 0; i < tagsContent.Length; i++)
             {
-                if (string.Equals(tagsContent[i].text, value))
-                {
-                    index = i;
-                }
+                tagsContent[i] = new GUIContent(tags[i]);
             }
-
-            label = EditorGUI.BeginProperty(position, label, property);
-            index = EditorGUI.Popup(position, label, index, tagsContent);
-            EditorGUI.EndProperty();
-
-            property.stringValue = tagsContent[index].text;
         }
-        else
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            EditorGUI.HelpBox(position,
-                string.Format(NOT_STRING_ERROR_FORMAT_MESSAGE, property.displayName),
-                MessageType.Error);
+            if (property.propertyType == SerializedPropertyType.String)
+            {
+                int index = 0;
+                string value = property.stringValue;
+                for (int i = 0; i < tagsContent.Length; i++)
+                {
+                    if (string.Equals(tagsContent[i].text, value))
+                    {
+                        index = i;
+                    }
+                }
+
+                label = EditorGUI.BeginProperty(position, label, property);
+                index = EditorGUI.Popup(position, label, index, tagsContent);
+                EditorGUI.EndProperty();
+
+                property.stringValue = tagsContent[index].text;
+            }
+            else
+            {
+                EditorGUI.HelpBox(position,
+                    string.Format(NOT_STRING_ERROR_FORMAT_MESSAGE, property.displayName),
+                    MessageType.Error);
+            }
         }
     }
 }
