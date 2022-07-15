9f644506958cff6b19bb9b3e500a660b6126b48c
Hyago Oliveira <hyagogow@gmail.com>
2022-07-15 18:11:29 -0300

    Add ReferenceComparableAttribute

---
+++ Editor/ComparableAttributes/ReferenceComparableAttribute.cs
@@ -0,0 +1,21 @@
+﻿namespace ActionCode.Attributes.Editor
+{
+    public class ReferenceComparableAttribute : IComparableAttribute
+    {
+        public readonly object other;
+        public readonly int current;
+
+        public ReferenceComparableAttribute(int current, object other)
+        {
+            this.other = other;
+            this.current = current;
+        }
+
+        public bool HasMetCondition()
+        {
+            if (other is null) return current != 0;
+            if (other is int intValue) return current == intValue;
+            return false;
+        }
+    }
+}
