45088f662518341d61db02047403ce071322bbe5
Hyago Oliveira <hyagogow@gmail.com>
2022-07-19 19:32:14 -0300

    Find inner properties

--- Editor/ComparableAttributes/AbstractComparableAttributeDrawer.cs
+++ Editor/ComparableAttributes/AbstractComparableAttributeDrawer.cs
@@ -8,7 +8,7 @@ namespace ActionCode.Attributes.Editor
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             var comparableAttibute = attribute as T;
-            var comparableProperty = property.serializedObject.FindProperty(comparableAttibute.property);
+            var comparableProperty = FindPropertyByPath(property, comparableAttibute.property);
 
             if (comparableProperty == null)
             {
@@ -25,5 +25,21 @@ namespace ActionCode.Attributes.Editor
         }
 
         protected abstract void DrawProperty(bool isCoditionMet, Rect position, SerializedProperty property, GUIContent label);
+
+        private SerializedProperty FindPropertyByPath(SerializedProperty property, string name)
+        {
+            const char pathSeparator = '.';
+            var pathSuffix = pathSeparator + property.name;
+            var newPathSuffix = pathSeparator + name;
+            var path = ReplaceLastPath(property.propertyPath, pathSuffix, newPathSuffix);
+            return property.serializedObject.FindProperty(path);
+        }
+
+        private static string ReplaceLastPath(string source, string oldValue, string newValue)
+        {
+            var place = source.LastIndexOf(oldValue);
+            if (place == -1) return newValue.Replace(".", string.Empty);
+            return source.Remove(place, oldValue.Length).Insert(place, newValue);
+        }
     }
 }
