c9ffb2543506e79ed5c213e7ba3410ba714874e8
Hyago Oliveira <hyagogow@gmail.com>
2025-09-29 16:47:23 -0300

    Add MinMaxLimitAttribute uxml file

--- Editor/MinMaxLimitAttributeDrawer.cs
+++ Editor/MinMaxLimitAttributeDrawer.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEditor;
+using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -8,9 +9,37 @@ namespace ActionCode.Attributes.Editor
     [CustomPropertyDrawer(typeof(MinMaxLimitAttribute))]
     public class MinMaxLimitAttributeDrawer : PropertyDrawer
     {
+        [SerializeField] private VisualTreeAsset asset;
+
         public override VisualElement CreatePropertyGUI(SerializedProperty property)
         {
-            var limit = (MinMaxLimitAttribute)attribute;
+            var root = asset.Instantiate();
+            var label = root.Q<Label>("Label");
+            var lowLimit = root.Q<Label>("LowLimit");
+            var highLimit = root.Q<Label>("HighLimit");
+            var lowValue = root.Q<FloatField>("LowValue");
+            var highValue = root.Q<FloatField>("HighValue");
+            var slider = root.Q<MinMaxSlider>("MinMaxSlider");
+            var minMaxLimit = attribute as MinMaxLimitAttribute;
+
+            label.text = property.displayName;
+            label.tooltip = property.tooltip;
+            lowLimit.text = minMaxLimit.min.ToString();
+            highLimit.text = minMaxLimit.max.ToString();
+
+            slider.lowLimit = minMaxLimit.min;
+            slider.lowLimit = minMaxLimit.max;
+            slider.BindProperty(property);
+
+            slider.RegisterValueChangedCallback(evt =>
+            {
+                lowValue.value = (int)evt.newValue.x;
+                highValue.value = (int)evt.newValue.y;
+
+                property.serializedObject.ApplyModifiedProperties();
+            });
+
+            /*var limit = (MinMaxLimitAttribute)attribute;
             var value = GetMinMaxValues(property);
             var container = new VisualElement();
             var slider = new MinMaxSlider(
@@ -19,14 +48,14 @@ namespace ActionCode.Attributes.Editor
                 value.y,
                 limit.min,
                 limit.max
-            );
+            );*/
 
             //container.style.flexDirection = FlexDirection.Row;
             //container.Add(new Label(limit.min.ToString()));
-            container.Add(slider);
+            //container.Add(slider);
             //container.Add(new Label(limit.max.ToString()));
 
-            return container;
+            return root;
         }
 
         private static Vector2 GetMinMaxValues(SerializedProperty property) => property.propertyType switch
